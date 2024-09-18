b9446cd50322e2d5fb9ef603e75cbb8d442e5344
Madigan Roozen <madigan.roozen@ucf.edu>
2024-09-18 13:14:52 -0400

    working on open_file and load_instructions
    open_file should now create a BOF object, call load_instructions, and close the file.
    load_instructions should read through the file and call read_instruction from instructions.c. Should put all instructions into memory.instrs.
    run should now print all instructions in memory.instrs.
    This all kind of works except it prints a lot of numbers so im not sure what those mean.

--- HW1/machine.c
+++ HW1/machine.c
@@ -6,6 +6,17 @@
 #include "instruction.h"
 #include "utilities.h"
 #include "machine.h"
+#include "bof.h"
+
+#define MEMORY_SIZE_IN_WORDS 32768
+#define NUM_REGISTERS 8
+
+//memory array for the VM
+static union mem_u{
+    word_type words[MEMORY_SIZE_IN_WORDS];
+    uword_type uwords[MEMORY_SIZE_IN_WORDS];
+    bin_instr_t instrs[MEMORY_SIZE_IN_WORDS];
+}memory;
 
 //initialize the VM
 //set initial values for fp, sp, pc
@@ -15,11 +26,42 @@ void initialize(){}
 
 //open bof
 //should call load_instructions and close file when done
-void open_file(){}
+//Madigan 9/18
+void open_file(const char *filename){
+
+    BOFFILE *f = malloc(sizeof(BOFFILE));
+    if (f == NULL) {
+    bail_with_error("Memory allocation failed");
+    }
+    f->filename = filename;
+    f->fileptr = fopen(filename, "rb");
+    if (f->fileptr == NULL) {
+    bail_with_error("Error opening file: %s", filename);
+    }
+
+    load_instructions(f);
+    //close file and handle errors
+    if(fclose(f->fileptr) != 0){
+        bail_with_error("Error closing file");
+    }
+
+}
 
 //read/decode and put instructions into memory
 //will probably use a lot from instructions file
-void load_instructions(){}
+void load_instructions(BOFFILE *f){
+
+    int num_instr = 0;
+    while (num_instr >= MEMORY_SIZE_IN_WORDS) {
+        bin_instr_t instr = instruction_read(*f);
+        memory.instrs[num_instr] = instr;
+        num_instr++;
+    }
+
+    if(num_instr >= MEMORY_SIZE_IN_WORDS){
+        bail_with_error("instr array full");
+    }
+}
 
 //do what the instruction says
 //move fp, sp and pc as needed
@@ -34,6 +76,11 @@ void trace(){}
 //call init, open, load, execute and trace 
 void run(const char *filename){
     printf("run has been called");
+    open_file(filename);
+    for(int i=0; i<sizeof(memory.instrs)/sizeof(memory.instrs[0]); i++){
+        instr_type t = instruction_type(memory.instrs[i]);
+        printf("%d", t);
+    }
 }
 
 //prints the current state of the memory stack
