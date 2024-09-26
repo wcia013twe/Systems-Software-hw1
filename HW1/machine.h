6dc3743fd2231b24f0a32ffb93912ab65f49eee1
Sparky-168 <sparkyrogers168@gmail.com>
2024-09-26 18:26:30 -0400

    Update machine.h
    added print_command function

--- HW1/machine.h
+++ HW1/machine.h
@@ -9,8 +9,8 @@
 //run the vm on the given file
 extern void run(const char *filename);
 
-//prints the state of the memory stack for debugging
-extern void print_state();
+//prints the instructions provided without executing
+void print_command (const char *filename);
 
 void load_instructions(BOFFILE *f);
 #endif
